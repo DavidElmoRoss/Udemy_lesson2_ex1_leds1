146b8c81e16aa6d002906657b0c3af0356b09f5c
David Elmo Ross <david.elmo.ross@icloud.com>
2024-07-07 18:33:44 +0000

    Udemy EX1_LEDS

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+    Title:  Program to Flash the RED LED on/off every second
+    Author: Your NAME, STUDENT ID, COURSE CODE, Lab Section
+    Date:   Today's Date
+    Description:    This program will turn the RED LED on and
+                    off every 1 second
+*/
+
+#include "mbed.h"                   // include mbed.h library
+
+int main()
+{
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led(LED1);
+
+    for(;;)
+     {
+        led = 1;                    // turn off RED LED
+        wait_ms(500);               // wait 500 ms
+        led = 0;                    // turn on RED LED
+       ThisThread::sleep_for(500);  // sleep for 500 ms
+    }
+}
